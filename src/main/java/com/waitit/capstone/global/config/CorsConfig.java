e16dcb7c9fd0828cdf64a822c6fe24ea687eb87d
HuitaePark <qkrgmlxo3174@gmail.com>
2025-04-12 00:52:39 +0900

    fix : cors 설정 수정 모든 오리진 접근 허용 추후 배포시 수정

--- src/main/java/com/waitit/capstone/global/config/CorsConfig.java
+++ src/main/java/com/waitit/capstone/global/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:19006")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
