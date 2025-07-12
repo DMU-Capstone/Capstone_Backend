d06768ce62914138635fbe43cc8982e63e5578d8
HuitaePark <qkrgmlxo3174@gmail.com>
2025-07-13 02:02:17 +0900

    feat : 배너서비스 배너조회 서비스 생성

--- src/main/java/com/waitit/capstone/domain/main/banner/BannerService.java
+++ src/main/java/com/waitit/capstone/domain/main/banner/BannerService.java
@@ -1,4 +1,20 @@
 package com.waitit.capstone.domain.main.banner;
 
+import com.waitit.capstone.domain.admin.dto.MainBannerResponse;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class BannerService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public MainBannerResponse getEventBanner() {
+        String redisKey = "main_banner";
+        List<String> list = redisTemplate.opsForList().range(redisKey, 0, 4);
+        return new MainBannerResponse("mainBanner", list);
+    }
 }
