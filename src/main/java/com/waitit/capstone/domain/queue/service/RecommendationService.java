593d021f206e1fc5812f624eb7092a35ef4cdb06
HuitaePark <qkrgmlxo3174@gmail.com>
2025-07-16 01:11:53 +0900

    feat : 장소 추천 서비스 생성

---
+++ src/main/java/com/waitit/capstone/domain/queue/service/RecommendationService.java
@@ -0,0 +1,11 @@
+package com.waitit.capstone.domain.queue.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecommendationService {
+    //좌표위치와 시간을 받아서 가야 할 장소 판단
+
+    //api 호출해서 지역 목록 반환
+
+}
