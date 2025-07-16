d316ba6f2bb2ebcec101409d30f0f415e64e5a1f
HuitaePark <qkrgmlxo3174@gmail.com>
2025-07-16 10:29:29 +0900

    feat : 시간을 판단하는 메서드 생성

--- src/main/java/com/waitit/capstone/domain/manager/dto/CoordinateDto.java
+++ src/main/java/com/waitit/capstone/domain/manager/dto/CoordinateDto.java
@@ -1,11 +1,5 @@
 package com.waitit.capstone.domain.manager.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class CoordinateDto {
-    private Double latitude;
-    private Double longitude;
+public record CoordinateDto(Double latitude,
+                            Double longitude) {
 }
--- src/main/java/com/waitit/capstone/domain/queue/service/RecommendationService.java
+++ src/main/java/com/waitit/capstone/domain/queue/service/RecommendationService.java
@@ -1,5 +1,6 @@
 package com.waitit.capstone.domain.queue.service;
 
+import com.waitit.capstone.domain.manager.dto.CoordinateDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,4 +9,19 @@ public class RecommendationService {
 
     //api 호출해서 지역 목록 반환
 
+    private boolean isOverOneHour(int minute){
+        return minute >= 60;
+    }
 }
+/** 30분
+ * 편의점 / 드럭스토어 (올리브영 등)
+ * 소품샵 / 액세서리 가게
+ * 서점 /
+ * 즉석 사진 부스 (인생네컷 등)
+ *
+ * ⏰ 1시간 웨이팅
+ * 근처 다른 카페
+ * 다이소 /서점
+ * 가까운 공원 또는 산책로
+ * 팝업 스토어 또는 작은 갤러리 (성수, 연남 등 핫플이라면!)
+ */
