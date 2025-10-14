72048dccd810b8e46c528b7351c3ae2bab705145
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-14 13:03:29 +0900

    fix: 오류 수정

--- src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyWaitingStats.java
+++ src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyWaitingStats.java
@@ -1,11 +1,15 @@
 package com.waitit.capstone.domain.dashboard.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class HourlyWaitingStats {
     private Integer hour;
     private Double averageQueueSize;
+
+    // JPQL의 new 생성자 표현식을 위해 명시적으로 생성자 추가
+    public HourlyWaitingStats(Integer hour, Double averageQueueSize) {
+        this.hour = hour;
+        this.averageQueueSize = averageQueueSize;
+    }
 }
