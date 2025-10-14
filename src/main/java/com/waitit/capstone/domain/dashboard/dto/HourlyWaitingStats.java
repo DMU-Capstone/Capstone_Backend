2a2f4fe2bf127dc775ddcfd49de7a05939d6ef80
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-14 14:30:57 +0900

    fix: dto 수정

--- src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyWaitingStats.java
+++ src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyWaitingStats.java
@@ -7,9 +7,8 @@ public class HourlyWaitingStats {
     private Integer hour;
     private Double averageQueueSize;
 
-    // JPQL의 new 생성자 표현식을 위해 명시적으로 생성자 추가
-    public HourlyWaitingStats(Integer hour, Double averageQueueSize) {
-        this.hour = hour;
-        this.averageQueueSize = averageQueueSize;
+    public HourlyWaitingStats(Number hour, Number averageQueueSize) {
+        this.hour = hour.intValue();
+        this.averageQueueSize = averageQueueSize.doubleValue();
     }
 }
