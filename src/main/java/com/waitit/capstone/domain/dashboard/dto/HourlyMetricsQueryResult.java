36b0ea05622b1912ca1dae311870e803fc6c1af1
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-20 13:37:28 +0900

    feat: dto 타입 수정

--- src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyMetricsQueryResult.java
+++ src/main/java/com/waitit/capstone/domain/dashboard/dto/HourlyMetricsQueryResult.java
@@ -1,19 +1,15 @@
 package com.waitit.capstone.domain.dashboard.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class HourlyMetricsQueryResult {
-    private final Integer hour;
+    private final Long hour;
     private final Long totalCount;
     private final Long enteredCount;
     private final Long cancelledCount;
 
-    public HourlyMetricsQueryResult(Integer hour, Long totalCount, Long enteredCount, Long cancelledCount) {
-        this.hour = hour;
-        this.totalCount = totalCount;
-        this.enteredCount = enteredCount;
-        this.cancelledCount = cancelledCount;
-    }
 }
